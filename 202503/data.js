806ba31be541d9a1c74a8a81528d55e7ecd38c9a
ssshArchive <archive_lib@sssh.tp.edu.tw>
2025-04-15 09:39:31 +0800

    202404
    新展覽

---
+++ 202503/data.js
@@ -0,0 +1,86 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "一樓漫研成果",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.23401929149123113,
+        "pitch": -0.02230108510611828,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.42491393647118,
+          "pitch": 0.19062852039297162,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1847389482691426,
+          "pitch": -0.048106811692399276,
+          "title": "女神",
+          "text": "漫研自創角色"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "一樓自主展覽",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.39601796381370313,
+        "pitch": 0.16926115239317063,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5526485541398962,
+          "pitch": 0.09512405940093593,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "3月松高圖書館展覽",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
