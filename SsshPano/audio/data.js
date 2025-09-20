80e7853bf220795c1f317d3ad3952c8273aabeca
haning47 <155292432+haning47@users.noreply.github.com>
2025-09-20 10:55:27 +0800

    ssshpano

---
+++ SsshPano/audio/data.js
@@ -0,0 +1,218 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-4",
+      "name": "視聽4樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.200894351691094,
+        "pitch": 0.08695865215041465,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.52643608105749,
+          "pitch": -0.14584827830623226,
+          "rotation": 0,
+          "target": "1-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-5",
+      "name": "視聽5樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0721308262251021,
+        "pitch": 0.19100143431434446,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6643873651217707,
+          "pitch": 0.0431418201631768,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.962260254612108,
+          "pitch": 0.3009627545804481,
+          "rotation": 0.7853981633974483,
+          "target": "0-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.936526720374296,
+          "pitch": -0.17615344467796135,
+          "title": "視聽大樓五樓",
+          "text": "語言教室一<div>語言教室二</div>"
+        },
+        {
+          "yaw": 0.9939844163145697,
+          "pitch": -0.29123193123507995,
+          "title": "電腦教室三",
+          "text": "高二生活科技<div>高三科技領域課程</div>"
+        },
+        {
+          "yaw": 1.9657504287010195,
+          "pitch": -0.08180247480781411,
+          "title": "電腦教室二",
+          "text": "高一資訊科技<div>高二進階程式設計</div><div>高三科技領域課程</div>"
+        }
+      ]
+    },
+	{
+      "id": "0-",
+      "name": "往視聽大樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.9424361212993624,
+        "pitch": -0.2510190679440818,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9611842985997603,
+          "pitch": -0.2322708906436759,
+          "rotation": 0,
+          "target": "0-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "電腦二內部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -3.0245486344338417,
+        "pitch": 0.1872247545846477,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.469533799210044,
+          "pitch": -0.03606293961537688,
+          "rotation": 7.853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "電二走廊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3495657077620269,
+          "pitch": -0.10114966082064214,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "視聽大樓",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ SsshPano/home/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "松山高中",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.553156761315357,
+        "pitch": -0.2857512635463344,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "松山高中720環景",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
