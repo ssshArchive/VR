10e6dbbaed137ef420bdedda51051b6acdfe9c90
haning47 <155292432+haning47@users.noreply.github.com>
2025-11-23 11:02:57 +0800

    home update

--- SsshPano/home/data.js
+++ SsshPano/home/data.js
@@ -16,16 +16,31 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": -1.553156761315357,
-        "pitch": -0.2857512635463344,
-        "fov": 1.3108730938557924
+        "yaw": 2.9727764557396776,
+        "pitch": -0.23053581739467077,
+        "fov": 1.0025330419681413
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -2.973940907161076,
+          "pitch": -0.27397618161354487,
+          "title": "松山高中簡介",
+          "text": "前身為臺北市松山初級中學<div>民國78年改制為高中</div><div>校慶為 11月12日</div><div>坐落在捷運市政府站附近</div><div>交通便利</div>"
+        }
+      ]
     }
   ],
   "name": "松山高中720環景",
@@ -33,6 +48,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
